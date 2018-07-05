99c64890972476110249646b7af651d0c4c6eec7
Chen Juan <e0253722@u.nus.edu>
2018-07-05 15:38:21 -0400

    More robust command line behavior

--- main.py
+++ main.py
@@ -4,24 +4,32 @@ import odb
 import sys
 
 
-def main(symbol):
-	settlements = sp.get_all_settlements(symbol)
-	print("settlements gotten")
-	options_data = tp.oi_tex_maker(settlements, symbol)
-	print("tex_made")
-	odb.add_data(symbol, settlements, options_data)
+def process(symbol):
+    settlements = sp.get_all_settlements(symbol)
+    print("{0} settlements gotten".format(sp.PRODUCT_SYMBOLS[symbol]["name"]))
+    options_data = tp.oi_tex_maker(settlements, symbol)
+    print("tex_made")
+    odb.add_data(symbol, settlements, options_data)
 
+
+def main(args):
+        
+    if len(args) == 0:
+        ps = sp.PRODUCT_SYMBOLS
+        for symbol in ps.keys():
+            print("{0}\t- {1}".format(symbol, ps[symbol]["name"]))
+        symbol = input("\nWhich commodity would you like to analyze? ")
+        process(symbol)
+        else:
+            pass
+    else:
+        for arg in args:
+            try:
+                process(arg)
+            except KeyError:
+                print("Invalid Product")
+                
 
 if __name__ == '__main__':
-    ps = sp.PRODUCT_SYMBOLS
 
-    for symbol in ps.keys():
-        print("{0}\t- {1}".format(symbol, ps[symbol]['name']))
-
-    symbol = input('\nWhich commmodity would you like to analyze?  ')
-
-#    try:
-    main(symbol)
-    #except KeyError:
-    #	print("Invalid Product")
-    #	sys.exit(1)
+    main(sys.args[1:])

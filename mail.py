9a4564cd6352288519dc440f9ec380b14c477b5d
Chen Juan <e0253722@u.nus.edu>
2018-07-13 12:27:57 -0400

    fixed send_mail bug

--- mail.py
+++ mail.py
@@ -10,6 +10,6 @@ def send_mail(text, subject, attachment):
     addressees = get_addressees()
     for addressee in addressees:
         try:
-            subprocess.run("echo \"{0}\" | mail -s \"{1}\" -A {2} {3}".format(text, subject, addressee, attachment), shell=True)
+            subprocess.run("echo \"{0}\" | mail -s \"{1}\" -A {2} {3}".format(text, subject, attachment, addressee), shell=True)
         except:
             pass
